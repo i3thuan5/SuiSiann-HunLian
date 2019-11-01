835fc9ef48fa1801f12934212cf13806ea09e7a6
fafoy <ihcaoe@gmail.com>
2019-11-01 10:50:10 +0800

    Siu tsuan-kehsik

--- NVIDIA/tacotron2-kehsik/tsuan.py
+++ NVIDIA/tacotron2-kehsik/tsuan.py
@@ -1,14 +1,16 @@
 from os.path import dirname, join
+from sys import argv
+from csv import DictReader
 
 
-def suisiann(csv_sootsai, boklok_sootsai):
+def main(csv_sootsai, boklok_sootsai):
     tsitma = dirname(csv_sootsai)
     tsuan = []
-    with open(csv_file, encoding='utf-8') as f:
+    with open(csv_sootsai, encoding='utf-8') as f:
         for tsua in DictReader(f):
             imtong = join(tsitma, tsua['音檔'])
             lmj = tsua['羅馬字']
-            tsuan.append('|'.join(imtong, lmj))
+            tsuan.append('|'.join([imtong, lmj]))
     with open(
         join(boklok_sootsai, 'suisiann_audio_text_train_filelist.txt'),
         'wt', encoding='utf-8'
@@ -24,8 +26,6 @@ def suisiann(csv_sootsai, boklok_sootsai):
         'wt', encoding='utf-8'
     ) as tong:
         print('\n'.join(tsuan[-500:]), file=tong)
-
-    return text_dict
 
 
 if __name__ == '__main__':
