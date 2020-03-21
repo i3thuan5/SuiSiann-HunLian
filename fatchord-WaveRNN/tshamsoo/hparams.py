159ae077660ff5752f508106de573f39a198b923
fafoy <ihcaoe@gmail.com>
2020-03-21 22:49:46 +0800

    16k e params kah dobi

--- fatchord-WaveRNN/tshamsoo/hparams.py
+++ fatchord-WaveRNN/tshamsoo/hparams.py
@@ -17,12 +17,12 @@ ignore_tts = False
 # DSP --------------------------------------------------------------------------------------------------------------#
 
 # Settings for all models
-sample_rate = 22050
+sample_rate = 16000
 n_fft = 2048
 fft_bins = n_fft // 2 + 1
 num_mels = 80
-hop_length = 275                    # 12.5ms - in line with Tacotron 2 paper
-win_length = 1100                   # 50ms - same reason as above
+hop_length = 200                    # 12.5ms - in line with Tacotron 2 paper
+win_length = 800                    # 50ms - same reason as above
 fmin = 40
 min_level_db = -100
 ref_level_db = 20
@@ -56,8 +56,8 @@ voc_clip_grad_norm = 4              # set to None if no gradient clipping needed
 
 # Generating / Synthesizing
 voc_gen_batched = True              # very fast (realtime+) single utterance batched generation
-voc_target = 11_000                 # target number of samples to be generated in each batch entry
-voc_overlap = 550                   # number of samples for crossfading between batches
+voc_target = 4_000                  # target number of samples to be generated in each batch entry
+voc_overlap = 400                   # number of samples for crossfading between batches
 
 
 # TACOTRON/TTS -----------------------------------------------------------------------------------------------------#
