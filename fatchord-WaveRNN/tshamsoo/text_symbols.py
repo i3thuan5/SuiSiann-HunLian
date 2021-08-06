66dcece2ce7bdc7d0ea4af90daab187593930806
Fafofafoy <ihcaoe@gmail.com>
2021-08-06 16:47:35 +0800

    加口語調欲--ê。

--- fatchord-WaveRNN/tshamsoo/text_symbols.py
+++ fatchord-WaveRNN/tshamsoo/text_symbols.py
@@ -3,17 +3,24 @@
 '''
 Defines the set of symbols used in text input to the model.
 
-The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
-from utils.text import cmudict
+The default is a set of ASCII characters that works well for English or text
+that has been run through Unidecode.
+See TRAINING_DATA.md for details.
+'''
 
 _pad = '_'
-_punctuation = '!\'(),.:;? '
-_special = '-'
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-_ipa = 'ŋəɛɨʔʰ̩ⁿ'
+_punctuation = r' -\.,;:?!"\'\(\)“”‘’~─'
+_letters = sorted(set(
+    'abcdefghijklmnopqrstuvwxyzáàâǎāa̍a̋éèêěēe̍e̋íìîǐīı̍i̍i̋'
+    'óòôǒōo̍őó͘ò͘ô͘ǒ͘ō͘o̍͘ő͘úùûǔūu̍űḿm̀m̂m̌m̄m̍m̋ńǹn̂ňn̄n̍n̋ⁿ'
+))
+_tsong = _letters + _letters.upper()
 
 _sooji = '0123456789'
+_piantiau = '規本隨再固三仔海'
 
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + list(_ipa) + list(_sooji)
-
+symbols = (
+    [_pad] + list(_punctuation)
+    + list(_tsong) + list(_sooji) + list(_piantiau)
+)
